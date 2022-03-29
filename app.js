c80a950a08cb5eed94676c12c4a3ea8868724fca
jorgedcb <jecb2001@gmail.com>
2022-03-29 15:38:26 -0500

    changing udp port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
  const mysql = require('mysql');
 var dgram = require('dgram');
-var port = 7000;
+var port = 5000;
 var port_web = 3000;
 const path = require('path')
 
