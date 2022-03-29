1723fc3c42b8983090deae46317aa1907174b6f7
jorgedcb <jecb2001@gmail.com>
2022-03-29 12:59:29 -0500

    i little change xd

--- README.md
+++ README.md
@@ -1,17 +1,43 @@
-# NodeJsApp-Part-1
-NodeJsApp 
+# nodejs-express-on-aws-ec2
 
+This repo hosts the source code for my YouTube tutorial on CI/CD from Github to an AWS EC2 instance via CodePipeline and CodeDeploy (https://www.youtube.com/watch?v=Buh3GjHPmjo). This tutorial uses a node.js express app as an example for the demo.
 
-### Clone and start by typing:
+I also created a video to talk about how to fix some of the common CodeDeploy failures I have run into (https://www.youtube.com/watch?v=sXZVkOH6hrA). Below are a couple of examples:
 
-``` bash
-# install dependencies
-$ npm install
-
-# serve with hot reload at localhost:3000
-$ npm start
-
-If you have any questions feel free to check out the youtube channel and post them in the comments. 
+```
+ApplicationStop failed with exit code 1
 ```
 
+```
+The overall deployment failed because too many individual instances failed deployment, too few healthy instances are available for deployment, or some instances in your deployment group are experiencing problems.
+```
 
+===========================
+
+EC2 script on creation to install the CodeDeploy Agent:
+
+```
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
+```
+
+Check if CodeDeploy agent is running:
+```
+sudo service codedeploy-agent status
+```
+
+Location for CodeDeploy logs:
+```
+/opt/codedeploy-agent/deployment-root/deployment-logs/codedeploy-agent-deployments.log
+```
+
+Uninstall CodeDeploy Agent:
+```
+sudo yum erase codedeploy-agent
+```
--- app.js
+++ app.js
@@ -1,75 +1,25 @@
-const express = require('express')
- const mysql = require('mysql');
-var dgram = require('dgram');
-var port = 5000;
-const path = require('path')
-
-require('dotenv').config();
-
-var data;
-var db = mysql.createConnection({
-  user: process.env.USER_DB,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  database: process.env.DB
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err
-    }
-      console.log('la conexión con la base de datos funciona')
-  });
-
+const express = require('express');
 const app = express();
-app.use(express.json())
+const port = 3000;
 
-// create table
-app.get('/creategpstable', (req, res) => {
-  let sql = 'CREATE TABLE mytable(id int AUTO_INCREMENT, latitud VARCHAR(255), longitud VARCHAR(255), fecha VARCHAR(255), hora VARCHAR(255), PRIMARY KEY(id))';
-  db.query(sql, (err,result) =>{
-    if(err) throw err;
-    console.log(result);
-    res.send('my table created...');
-  });
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+})
 
-app.get('/home', (req,res) => {
-  let sql = 'SELECT * FROM mytable ORDER BY id DESC LIMIT 1';
-  let query = db.query(sql,(err, results) =>{
-    if(err) throw err;
-    res.send(results[0]);
-  });
-});
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
 
-app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "home.html"));
-});
-
-socket = dgram.createSocket('udp4');
-
-socket.on('message', function (msg, info){
-    datagps = msg.toString().split(';')
-    console.log(datagps);
-    var time = datagps[1].split(' ')
-    var coordenadas = datagps[0].split('-')
-    let lon = '-';
-    let long = lon.concat(coordenadas[1]);
-    let post = {latitud:coordenadas[0], longitud:long, fecha:time[0], hora:time[1]};
-    let sql = 'INSERT INTO mytable set ?';
-    let query = db.query(sql, post,(err, result) => {
-      if(err) throw err;
-      console.log("mesage insetado")
-    })
-  });
-
-socket.on('listening', function(){
-    var address = socket.address();
-    console.log("listening on :" + address.address + ":" + address.port);
-});
-
-socket.bind(port);
-
-app.listen('3000', () => {
-    console.log('server started on port 3000')
-});
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
